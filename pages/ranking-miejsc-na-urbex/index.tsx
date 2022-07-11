f7ff9e19cb0d57ec7cbef11112a9e00c77e368d8
Szymon Chwila <Szymon_Chwila@macbook-pro-bozena.home>
2022-07-11 14:27:36 +0200

    ranking is already done but needs sone styling

--- pages/ranking-miejsc-na-urbex/index.tsx
+++ pages/ranking-miejsc-na-urbex/index.tsx
@@ -2,20 +2,112 @@ import React from 'react';
 import styles from './index.module.scss';
 import HorizontalArticleCard from '../../src/components/ArticleCard/HorizontalArticleCard';
 import photo1 from '../../public/assets/urbexPhotos/urbexPhoto1.webp';
+import { sanityClient } from '../../sanity';
+import { AiFillStar } from 'react-icons/ai';
+import Link from 'next/link';
 
-function Index() {
+const filledStarScore = (ourRating) => {
+  let result = [];
+  for (let i = 1; i <= 10; i++) {
+    if (i <= ourRating) {
+      result.push(
+        <AiFillStar
+          className={styles.star}
+          style={{
+            fill: 'rgb(240, 240, 0)',
+            stroke: '#000',
+            strokeWidth: '20'
+          }}
+        />
+      );
+    } else {
+      result.push(
+        <AiFillStar
+          className={styles.star}
+          style={{
+            fill: 'rgb(255, 255, 200)',
+            stroke: '#000',
+            strokeWidth: '20'
+          }}
+        />
+      );
+    }
+  }
+
+  return result;
+};
+
+const RankItem = ({ placeName, ourRating, slug, city, date, nr }) => {
   return (
-    <div className='container'>
-      <HorizontalArticleCard
-        title='Fort Kraków'
-        description='Urbex (z ang. urban exploration — eksploracja miejska) — forma
-          aktywności polegająca na eksplorowaniu opuszczonych, zrujnowanych,
-          zapomnianych, niedostępnych czy ukrytych budynków i instalacji'
-        imgAlt='photo from an urbex'
-        imgUrl={photo1}
-      />
+    <div className={styles.rankItemContainer}>
+      <div className={styles.rankNumber}>{nr}</div>
+      <Link href={`/miejsca-na-urbex/${slug}`}>
+        <div className={styles.place}>
+          <div className={styles.placeInfo}>
+            <h3 className={styles.placeName}>{placeName}</h3>
+            <p className={styles.city}>{city}</p>
+            <p className={styles.date}>{date}</p>
+          </div>
+          <div className={styles.ourRating}>
+            {filledStarScore(ourRating)}
+            <p>{ourRating}/10</p>
+          </div>
+        </div>
+      </Link>
     </div>
   );
+};
+
+function Index({ places }) {
+  return (
+    <main>
+      <section className={styles.landingSection}>
+        <div className={styles.landingPage}>
+          <header>
+            <div>
+              <h1>
+                Zobacz ranking najlepszych miejsc na urbex, których nasza grupa
+                urbexowa podjęła się eksploracji
+              </h1>
+            </div>
+          </header>
+        </div>
+      </section>
+
+      <div className='container'>
+        <section className={styles.section}>
+          {places.map((el, index) => (
+            <RankItem
+              placeName={el.placeName}
+              ourRating={el.ourRating}
+              slug={el.article.slug.current}
+              city={el.city.city}
+              date={el.date}
+              nr={index + 1}
+            />
+          ))}
+        </section>
+      </div>
+    </main>
+  );
 }
+
+export const getStaticProps = async (context: any) => {
+  const places = await sanityClient.fetch(`
+    *[_type == "place"] | order(ourRating desc)[] {
+      placeName,
+      city->{city},
+      ourRating,
+      article->{slug},
+      date
+    }[0..9]
+  `);
+
+  return {
+    props: {
+      places
+    }
+  };
+};
 
 export default Index;
